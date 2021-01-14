70358a3940860e95b384608e9cbf8196c88218e5
Tomoyuki Sakurai <y@trombik.org>
2021-01-15 06:07:08 +0700

    update to 2020r3

--- devel/xtensa-lx106-elf/files/patch-scripts_build_internals.sh
+++ devel/xtensa-lx106-elf/files/patch-scripts_build_internals.sh
@@ -1,6 +1,6 @@
---- scripts/build/internals.sh.orig	2019-11-11 06:13:50 UTC
+--- scripts/build/internals.sh.orig	2021-01-13 05:48:34 UTC
 +++ scripts/build/internals.sh
-@@ -13,7 +13,7 @@ do_finish() {
+@@ -43,7 +43,7 @@ do_finish() {
  
      if [ "${CT_STRIP_HOST_TOOLCHAIN_EXECUTABLES}" = "y" ]; then
          case "$CT_HOST" in
